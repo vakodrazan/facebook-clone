b72f3f734823352f029a78bf2dbb7fef31b747b5
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-11-30 10:58:08 +0300

    Style the header element

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,61 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderStyle = styled.header`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    .heading {
+        font-weight: normal;
+        font-size: 18px;
+    }
+
+    .list {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        width: 70%;
+        font-size: 16px;
+
+        li {
+            padding: 3px;
+        }
+
+        img {
+            border-radius: 100%;
+            width: 46px;
+            height: 46px;
+        }
+
+        .userName {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+
+            span {
+                padding-right: 10px;
+            }
+        }
+    }
+`;
 
 function Header() {
     return (
-        <header>
-            <h1>OnjaBook</h1>
-            <ul>
+        <HeaderStyle>
+            <h1 className="heading">OnjaBook</h1>
+            <ul className="list">
                 <li>Feed</li>
                 <li>Add post</li>
-                <li>
+                <li className="userName">
                     <span>Noeline Marie</span>
                     <img src="https://iili.io/Fwvaat.jpg" alt="Noeline Marie" />
                 </li>
             </ul>
-        </header>
+        </HeaderStyle>
     )
 }
 
